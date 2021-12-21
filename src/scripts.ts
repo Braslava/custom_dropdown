edb415bd8dc4bcca9e46a24f6739d9e0e08d9f32
Your Name <m.braslava@gmail.com>
2021-12-21 12:11:55 +0200

    fixed displaying slected option with event delegation

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,73 +1,51 @@
+const dropdownContainer = document.querySelector('.js-dropdown-container');
+const openBtn = dropdownContainer.querySelector('.js-open-btn');
+const dropdownMenu = dropdownContainer.querySelector('.js-menu');
+const selectedItem = document.querySelector('.js-selected-item');
 
+const choices = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5'];
 
-const dropdownContainer = document.querySelector('.js-dropdown-container'); 
-const openBtn = dropdownContainer.querySelector('.js-open-btn'); 
-const dropdownMenu = dropdownContainer.querySelector('.js-menu'); 
-const selectedTextBox = dropdownContainer.querySelector('.js-dropdown-sidebar'); 
-let selection: HTMLParagraphElement = dropdownContainer.querySelector('.selection'); 
-
-
-const choices = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5']; 
-
+/* Helper functions for opening/closing the dropdown */
 function openDropdown() {
-    dropdownMenu.classList.add('show'); 
+  dropdownMenu.classList.add('show');
 }
 
-function closeDropdown(){
-    dropdownMenu.classList.remove('show'); 
+function closeDropdown() {
+  dropdownMenu.classList.remove('show');
 }
+
+/* Events */
 
 openBtn.addEventListener('click', () => {
-    if (dropdownMenu.classList.contains('show')) {
-        closeDropdown();  
-    } else {
-        openDropdown(); 
-    }
-}); 
-const displayValues: HTMLParagraphElement[] = []; 
-
-
-const menuOptions: HTMLButtonElement[] = choices.map((choice, index) => {
-    const menuOption = document.createElement("button"); 
-    const optionText = document.createTextNode(`${choices[index]}`); 
-    menuOption.appendChild(optionText); 
-    menuOption.classList.add('option'); 
-    dropdownMenu.appendChild(menuOption);
-
-    const displayValue = document.createElement("p"); 
-    const displayText = document.createTextNode(`Slected option: ${choices[index]}`);
-    displayValue.appendChild(displayText); 
-    selectedTextBox.appendChild(displayValue)
-    displayValue.classList.add('selection');
-
-    displayValues.push(displayValue);
-
-    return menuOption;
-}); 
-
-let activeItem: HTMLParagraphElement; 
-
-menuOptions.forEach((menuOption, i) => {
-    menuOption.addEventListener('click', () => {
-    // Need to remove 'show' class from already selected options 
-    if (!!activeItem === true) {
-        activeItem.classList.remove('show'); 
-    }
-    displayValues[i].classList.add('show'); 
-    displayValues[i] = activeItem; 
+  if (dropdownMenu.classList.contains('show')) {
     closeDropdown();
-    }); 
-}
-   
-)
-
-
-document.addEventListener('click', (event) => {
-    const element = event.target as HTMLElement; 
-    const clickedOutsideElement = !!element.closest('.dropdown'); 
-    if (clickedOutsideElement === false) {
-        closeDropdown();
-    }
+  } else {
+    openDropdown();
+  }
 });
-  
-   
+
+// create the options and add them to the dropdown
+choices.map((choice, index) => {
+  const menuOption = document.createElement('button');
+  menuOption.innerHTML = `${choices[index]}`;
+  menuOption.classList.add('option');
+  dropdownMenu.appendChild(menuOption);
+  return menuOption;
+});
+
+// display the selection
+dropdownMenu.addEventListener('click', (event) => {
+  const clickedOption = event.target as HTMLElement;
+  if (clickedOption.nodeName === 'BUTTON') {
+    selectedItem.textContent = clickedOption.innerText;
+  }
+});
+
+// closes the dropdown when clicked outside
+document.addEventListener('click', (event) => {
+  const element = event.target as HTMLElement;
+  const clickedOutsideElement = !!element.closest('.dropdown');
+  if (clickedOutsideElement === false) {
+    closeDropdown();
+  }
+});
